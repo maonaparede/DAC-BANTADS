4e989628ce7147c9d56d4d0b932d16e58ba782c0
Arthur Gian <89954777+arthurgian@users.noreply.github.com>
2023-04-09 11:57:33 -0300

    Some changes in login page

--- Bantads/src/app/pages/login/login.component.ts
+++ Bantads/src/app/pages/login/login.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss', './login.component.utils.scss']
 })
 export class LoginComponent { }
