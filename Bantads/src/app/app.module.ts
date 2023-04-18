9070aebb68f192fb4358f32c31327ed195505dde
diano <alexandrediano.souza@outlook.com>
2023-04-17 21:03:11 -0300

    corrigido detalhes TS do register

--- Bantads/src/app/app.module.ts
+++ Bantads/src/app/app.module.ts
@@ -1,27 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { RegisterComponent } from "./pages/register/register.component";
+import { ClientHomeComponent } from "./pages/client/home/home.component";
+import { ManagerHomeComponent } from "./pages/manager/home/home.component";
+import { AdminHomeComponent } from "./pages/admin/home/home.component";
+import { ListComponent } from "./pages/admin/list/list.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './pages/login/login.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from "@angular/material/button";
-import {MatCardModule} from "@angular/material/card";
-import {MatInputModule} from "@angular/material/input";
-import {MatSelectModule} from "@angular/material/select";
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {RegisterComponent} from "./pages/register/register.component";
-import { ClientHomeComponent } from './pages/client/home/home.component';
-import { ManagerHomeComponent } from './pages/manager/home/home.component';
-import { AdminHomeComponent } from './pages/admin/home/home.component';
-import { ListComponent } from './pages/admin/list/list.component';
-
-import { NgxMaskModule } from 'ngx-mask'
+import { NgxMaskModule } from "ngx-mask";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -47,8 +47,9 @@ import { NgxMaskModule } from 'ngx-mask'
     MatSelectModule,
     FlexLayoutModule,
     NgxMaskModule.forRoot(),
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Bantads/src/app/pages/register/register.component.ts
+++ Bantads/src/app/pages/register/register.component.ts
@@ -1,7 +1,66 @@
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { Router } from "@angular/router";
+import { consultarCep } from "correios-brasil";
+
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  form: FormGroup;
+  address = "";
+  neighborhood = "";
+  city = "";
+  state = "";
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.form = this.formBuilder.group({
+      name: new FormControl("", Validators.required),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      // password: new FormControl('', [
+      //   Validators.required,
+      //   Validators.pattern(
+      //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$/
+      //   ),
+      // ]),
+      // passwordConfirmation: new FormControl('', [
+      //   Validators.required,
+      //   Validators.pattern(
+      //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$/
+      //   ),
+      // ]),
+      role: new FormControl("User", Validators.required),
+      cpf: new FormControl("", [Validators.required]),
+      phone: new FormControl("", [Validators.required]),
+      salary: new FormControl("", Validators.required),
+      state: new FormControl("", Validators.required),
+      city: new FormControl({ value: "", disabled: true }, Validators.required),
+      cep: new FormControl("", [
+        Validators.required,
+        Validators.pattern(/^(\d{2}\.)\d{3}\-\d{3}$/),
+        Validators.minLength(10),
+        Validators.maxLength(10),
+      ]),
+      street: new FormControl("", Validators.required),
+      number: new FormControl("", Validators.required),
+      complement: new FormControl(""),
+    });
+  }
+
+  getAddress(cep: string) {
+    cep = cep.replace(/\D/g, "");
+    consultarCep(cep).then((response) => {
+      this.neighborhood = response.bairro;
+      this.address = response.logradouro;
+      this.city = response.localidade;
+      this.state = response.uf;
+    });
+  }
+}
