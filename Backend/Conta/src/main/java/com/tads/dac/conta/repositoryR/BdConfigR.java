fb5fc11175b693bbcb0f1e2a66a7285221eabc1b
maonaparede <jefferson9312@gmail.com>
2023-06-14 22:18:33 -0300

    tds os modulos agr funcionam

--- Backend/Conta/src/main/java/com/tads/dac/conta/repositoryR/BdConfigR.java
+++ Backend/Conta/src/main/java/com/tads/dac/conta/repositoryR/BdConfigR.java
@@ -44,8 +44,7 @@ public class BdConfigR {
         em.setJpaVendorAdapter(vendorAdapter);
         final HashMap<String, Object> properties = new HashMap<String, Object>();
         
-        properties.put("hibernate.dialect",
-                env.getProperty("org.hibernate.dialect.PostgreSQL81Dialect"));
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL81Dialect");
         
         properties.put("hibernate.hbm2ddl.auto", "update");
         
@@ -65,8 +64,8 @@ public class BdConfigR {
           = new DriverManagerDataSource();
         
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("postgresql://bantads-db:5432/bantads");
-        dataSource.setSchema("contaR");
+        dataSource.setUrl("jdbc:postgresql://bantads-db:5432/postgres");
+        dataSource.setSchema("contar");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres");
         
