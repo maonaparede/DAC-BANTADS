b004e169822077e8e4e8a3c210f9799cb6a6e03b
maonaparede <jefferson9312@gmail.com>
2023-06-16 13:39:29 -0300

    Só inserir ger n funciona

--- Backend/Cliente/init.sql
+++ Backend/Cliente/init.sql
@@ -1,26 +1,2 @@
 DROP DATABASE IF EXISTS cliente;
 
-CREATE DATABASE cliente;
-
-\c cliente;
-
-CREATE TABLE cliente (
-    id bigint PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    senha VARCHAR(1000) NOT NULL,
-    cpf VARCHAR(20) NOT NULL,
-    telefone VARCHAR(20) NOT NULL,
-    estado INT NOT NULL,
-    cidade INT NOT NULL,
-    cep VARCHAR(20) NOT NULL,
-    rua VARCHAR(255) NOT NULL,
-    numero INT NOT NULL,
-    complemento VARCHAR(255),
-    ativo BOOLEAN NOT NULL,
-    cargo VARCHAR(20) NOT NULL
-);
-
-INSERT INTO cliente (id,nome, email, senha, cpf, telefone, estado, cidade, cep, rua, numero, complemento, ativo, cargo) VALUES
-    (1,'CLIENTE 1', 'cliente1@cliente.com', '059bd4278d57c805cd939d76f30b26ef80d420c2a0662e88e2e75f8002bd62c7cd90bf3144d4d6cae3d979358360ede1d0dca9d2b66438acf59fdb30dcb9631f', '765.141.530-20', '(41) 99788-1313', 25, 3426, '88.465-450', 'Rua do Cliente 1', 15, null, TRUE, 'Cliente'),
-    (2,'CLIENTE 2', 'cliente2@cliente.com', 'fc51872ca532fac69d0ce2f73dc2f25cc2687504e1954b376a76dcde2f0429d830e6e58b49056724e8a9664b09c5f3ef7df492559d406dd123651e7a2437fd64', '246.905.310-21', '(41) 97894-2136', 25, 3426, '87.422-220', 'Rua do Cliente 2', 10, null, TRUE, 'Cliente');
--- Backend/Gerente/init.sql
+++ Backend/Gerente/init.sql
@@ -1,23 +1,2 @@
 DROP DATABASE IF EXISTS gerente;
 
-CREATE DATABASE gerente;
-
-\c gerente;
-
-CREATE TABLE gerente (
-	id BIGINT PRIMARY KEY,
-	nome VARCHAR(255) NOT NULL,
-	email VARCHAR(50) NOT NULL,
-	senha VARCHAR(1000) NOT NULL,
-	cpf VARCHAR(20) NOT NULL,
-	telefone VARCHAR(20) NOT NULL,
-	cargo VARCHAR(20) NOT NULL
-);
-
-INSERT INTO gerente (id,nome, email, senha, cpf, telefone, cargo) VALUES
-	(6,'GERENTE 1', 'gerente1@gerente.com', '5b0f380c06da974c8591432bb24fc713f2a019098b161112c52eca0db5ecd8d9833319330308bab56cd98eb253b0891f4c53deddc2c4857dd958571c3d564dee', '765.141.530-20', '(41) 99788-1313', 'Gerente'),
-	(7,'GERENTE 2', 'gerente2@gerente.com', 'b18f6b920da857f0e3858fc59ee084686c5175a9598f19f38f2ae3017781d1ef4447ef07bae613f13fd8b597e1e51ed9e85fe437e79beb15de037b7b02e086b2', '865.229.210-81', '(41) 99761-1414', 'Gerente');
-
--- Senhas
--- Gerente1.
--- Gerente2.
