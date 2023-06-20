b3f420c71d3fa20d4ae3b366d20b754e98abbfab
maonaparede <jefferson9312@gmail.com>
2023-06-20 16:43:23 -0300

    att

---
+++ Backend/Auth/src/main/java/com/tads/dac/auth/controller/TestController.java
@@ -0,0 +1,16 @@
+
+package com.tads.dac.auth.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    //Só pra testar o api gateway
+    @GetMapping("/")
+    public ResponseEntity<?> helloworld(){ 
+        return new ResponseEntity<>("hello world", HttpStatus.OK); 
+    }
+}
