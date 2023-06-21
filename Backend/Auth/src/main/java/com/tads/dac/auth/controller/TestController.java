be33c1595f48a2f7bf02a30dacff86ad989cf087
maonaparede <jefferson9312@gmail.com>
2023-06-21 19:58:52 -0300

    testa

--- Backend/Auth/src/main/java/com/tads/dac/auth/controller/TestController.java
+++ Backend/Auth/src/main/java/com/tads/dac/auth/controller/TestController.java
@@ -3,10 +3,12 @@ package com.tads.dac.auth.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5000")
 public class TestController {
     //Só pra testar o api gateway
     @GetMapping("/")
